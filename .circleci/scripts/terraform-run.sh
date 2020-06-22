64d0607ff6c6fe8ff88b7b5e878e2d81d2bba1db
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-22 14:20:08 -0300

    initial tests

--- .circleci/scripts/terraform-run.sh
+++ .circleci/scripts/terraform-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Applying changes to ${TERRAFORM_ENV} ${ENVIRONMENT}"
+
+cd src && AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} make plan
